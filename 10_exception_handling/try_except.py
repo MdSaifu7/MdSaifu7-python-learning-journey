1034003e0abd543277d097110f6c2854a9cb57ca
MdSaifu7 <147339573+MdSaifu7@users.noreply.github.com>
2026-01-09 22:16:43 +0530

    Implement exception handling in server_chai function
    Learn topics (1)Creating custom error 
    (2) How data flow in try , except, else and finally block

--- 10_exception_handling/try_except.py
+++ 10_exception_handling/try_except.py
@@ -1,1 +1,16 @@
 
+def server_chai(flavor):
+    print(f"Preparing {flavor} chai")
+    try:
+        if flavor == "unknown":
+            raise ValueError("Given flavour is not available")
+    except ValueError as e:
+        print("Error : ", e)
+    else:
+        print(f"Serving {flavor} chai")
+    finally:
+        print("Next costumer pleas.....")
+
+
+server_chai("Ginger")
+server_chai("unknown")
